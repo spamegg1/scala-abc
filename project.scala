6f8993610d5b6715976259ce006f0befc9d7c84f
spamegg1 <spamegg@pm.me>
2024-02-11 22:40:35 +0300

    add project.scala and .gitignore

---
+++ project.scala
@@ -0,0 +1,16 @@
+//> using scala 3.3.1
+//> using platform jvm scala-native
+//> using resourceDir ./src/main/resources
+//> using test.resourceDir ./test/main/resources
+//> using toolkit default
+//> using dep org.scalafx::scalafx:21.0.0-R32
+//> using dep org.creativescala::doodle:0.21.0
+//> using dep net.ruippeixotog::scala-scraper:3.1.1
+//> using dep io.spray::spray-json:1.3.6
+//> using dep org.scala-lang.modules::scala-xml:2.2.0
+//> using dep org.xerial:sqlite-jdbc:3.45.1.0
+//> using dep com.typesafe.slick::slick:3.5.0-RC1
+//> using dep org.scala-graph:graph-core_2.13:2.0.1
+//> using dep com.github.cascala:galileo_2.13:0.1.3
+//> using dep at.logic.gapt:gapt_2.13:2.16.0
+//> using dep org.axle-lang:axle-awt_2.13:0.6.6
