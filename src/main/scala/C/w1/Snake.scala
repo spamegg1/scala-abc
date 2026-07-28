cf5c1b540cc2808c2def0c6718e42846e0516216
spamegg <spamegg@pm.me>
2026-07-28 13:06:46 +0300

    basic Snake with ScalaFX

--- src/main/scala/C/w1/Snake.scala
+++ src/main/scala/C/w1/Snake.scala
@@ -1,6 +1,10 @@
+package C.w1
+package snake
+
 import scalafx.application.{JFXApp3, Platform}
 import scalafx.beans.property.{IntegerProperty, ObjectProperty}
 import scalafx.scene.Scene
+import javafx.scene.input.KeyEvent
 import scalafx.scene.paint.Color
 import scalafx.scene.paint.Color.*
 import scalafx.scene.shape.Rectangle
@@ -9,11 +13,61 @@ import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.Random
 
-object SnakeFx extends JFXApp3:
-  type Food = (Double, Double)
-  type Snake = List[Food]
+type Food = (x: Double, y: Double)
+type Snake = List[Food]
 
-  val initialSnake: Snake = List((250.0, 200.0), (225.0, 200.0), (200.0, 200.0))
+object Food:
+  def random: Food = (x = Random.nextInt(24) * 25, y = Random.nextInt(24) * 25)
+  extension (f: Food)
+    def next(dir: Int): Food = dir match
+      case 1 => (f.x, f.y - 25)
+      case 2 => (f.x, f.y + 25)
+      case 3 => (f.x - 25, f.y)
+      case 4 => (f.x + 25, f.y)
+      case _ => (f.x, f.y)
+
+object Snake:
+  val init: Snake = List((250.0, 200.0), (225.0, 200.0), (200.0, 200.0))
+  extension (s: Snake)
+    def check(f: Food) =
+      f.x < 0 || f.x >= 600 || f.y < 0 || f.y >= 600 || s.tail.contains((f.x, f.y))
+
+object State:
+  val init = State(Snake.init, Food.random)
+  def square(xr: Double, yr: Double, color: Color) = new Rectangle:
+    x = xr
+    y = yr
+    width = 25
+    height = 25
+    fill = color
+end State
+
+case class State(snake: Snake, food: Food):
+  def rectangles: List[Rectangle] =
+    State.square(food.x, food.y, Red) ::
+      snake.map((x, y) => State.square(x, y, Green))
+
+  def newSnake(f: Food): Snake =
+    import Snake.check
+    if snake.check(f) then Snake.init
+    else if food == f then food :: snake
+    else f :: snake.init
+
+  def newFood(f: Food) = if food == f then Food.random else food
+
+  def newState(dir: Int): State =
+    import Food.next
+    val f = snake.head.next(dir)
+    State(newSnake(f), newFood(f))
+end State
+
+object SnakeFx extends JFXApp3:
+  def controls(dir: IntegerProperty, key: KeyEvent) =
+    key.getText match
+      case "w" => dir.value = 1
+      case "s" => dir.value = 2
+      case "a" => dir.value = 3
+      case "d" => dir.value = 4
 
   def gameLoop(update: () => Unit): Unit =
     Future:
@@ -21,47 +75,14 @@ object SnakeFx extends JFXApp3:
       Thread.sleep(1000 / 25 * 2)
     .flatMap(_ => Future(gameLoop(update)))
 
-  case class State(snake: Snake, food: Food):
-    def check(newx: Double, newy: Double) =
-      newx < 0 || newx >= 600 || newy < 0 || newy >= 600 ||
-        snake.tail.contains((newx, newy))
-
-    def newState(dir: Int): State =
-      val (x, y) = snake.head
-      val (newx, newy) = dir match
-        case 1 => (x, y - 25)
-        case 2 => (x, y + 25)
-        case 3 => (x - 25, y)
-        case 4 => (x + 25, y)
-        case _ => (x, y)
-
-      val newSnake: Snake =
-        if check(newx, newy) then initialSnake
-        else if (food == (newx, newy)) food :: snake
-        else (newx, newy) :: snake.init
-
-      val newFood = if food == (newx, newy) then randomFood else food
-      State(newSnake, newFood)
-
-    def rectangles: List[Rectangle] =
-      square(food._1, food._2, Red) ::
-        snake.map((x, y) => square(x, y, Green))
-
-  def randomFood: Food = (Random.nextInt(24) * 25, Random.nextInt(24) * 25)
-
-  def square(xr: Double, yr: Double, color: Color) = new Rectangle:
-    x = xr
-    y = yr
-    width = 25
-    height = 25
-    fill = color
-
   override def start(): Unit =
-    val state = ObjectProperty(State(initialSnake, randomFood))
+    val state = ObjectProperty(State.init)
     val frame = IntegerProperty(0)
     val direction = IntegerProperty(4) // right
+
     frame.onChange:
-      state.update(state.value.newState(direction.value))
+      state.update:
+        state.value.newState(direction.value)
 
     stage = new JFXApp3.PrimaryStage:
       width = 600
@@ -69,15 +90,9 @@ object SnakeFx extends JFXApp3:
       scene = new Scene:
         fill = White
         content = state.value.rectangles
-        onKeyPressed = key =>
-          key.getText match
-            case "w" => direction.value = 1
-            case "s" => direction.value = 2
-            case "a" => direction.value = 3
-            case "d" => direction.value = 4
-
-        state.onChange(Platform.runLater:
-          content = state.value.rectangles
-        )
+        onKeyPressed = key => controls(direction, key)
+        state.onChange:
+          Platform.runLater:
+            content = state.value.rectangles
 
     gameLoop(() => frame.update(frame.value + 1))
