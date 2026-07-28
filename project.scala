04623de9f19ecaafd2af03351691b6c7ab0e525d
spamegg <spamegg@pm.me>
2026-07-28 22:28:30 +0300

    suppress worksheet warnings

--- project.scala
+++ project.scala
@@ -1,9 +1,10 @@
 //> using scala 3.8.4
 //> using jvm 24
-//> using nativeVersion 0.5.10
+//> using nativeVersion 0.5.12
 //> using resourceDir ./src/main/resources
 //> using test.resourceDir ./test/main/resources
 //> using toolkit 0.9.2
+//> using options -Wconf:msg=classpath:s -Wconf:msg=pure:s
 //> using dep org.scala-lang.modules::scala-parallel-collections::1.2.0
 //> using dep org.scalafx::scalafx:24.0.0-R35
 //> using dep org.creativescala::doodle:0.34.0
