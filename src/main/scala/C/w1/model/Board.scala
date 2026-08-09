c68489923e99e615d299bcabdc55858077b92dee
spamegg <spamegg@pm.me>
2026-08-09 15:33:00 +0300

    continue Board: removeFilled

--- src/main/scala/C/w1/model/Board.scala
+++ src/main/scala/C/w1/model/Board.scala
@@ -10,12 +10,23 @@ case class Board(pieces: List[Piece]):
   var currentPos   = Seq[Option[Color]]()
   val grid = Array.fill(Constants.Rows)(Array.ofDim[Option[Color]](Constants.Cols))
 
+  /** Takes a point and checks to see if it is in the bounds of the board and currently
+    * empty.
+    *
+    * @param pos
+    *   An (x, y) coordinate on the grid.
+    * @return
+    *   true if position is empty, false otherwise.
+    */
   def emptyAt(pos: Pos) =
     if pos.x < 0 || pos.x >= Constants.Cols then false
     else if pos.y < 1 then true
     else if pos.y >= Constants.Rows then false
     else !grid(pos.y)(pos.x).isDefined
 
+  /** Gets the information from the current piece about where it is and uses this to store
+    * the piece on the board itself. Then calls `removeFilled``.
+    */
   def storeCurrent: Unit =
     val locations    = currentBlock.currentRot
     val displacement = currentBlock.position
@@ -25,4 +36,16 @@ case class Board(pieces: List[Piece]):
     removeFilled
     delay = math.max(80, delay - 2)
 
-  def removeFilled = ()
+  /** Removes all filled rows and replaces them with empty ones, dropping all rows above
+    * them down each time a row is removed and increasing the score.
+    */
+  def removeFilled =
+    import Constants.{Rows, Cols}
+    (2 until Rows).foreach: row =>
+      if grid(row).forall(_.isDefined) then // see if this row is full (has no None)
+        (0 until Cols).foreach: colIndex => // remove from canvas blocks in full row
+          grid(row)(colIndex) = None
+        (Rows - row + 1 until Rows).foreach: row2 => // move down all rows above
+          grid(Rows - row2 + 1) = grid(Rows - row2)
+        grid(0) = Array.fill[Option[Color]](Cols)(None) // insert new blank row at top
+        score += 10                                     // adjust score for full flow
